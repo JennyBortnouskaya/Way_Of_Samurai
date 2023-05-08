7af2e05aed5f73938d814ffde176f0f073d3e454
gienie.b17 <gienie.b17@gmail.com>
2023-05-08 21:28:26 +0300

    Lesson 62 - API for follow/unfollow

--- way_of_samurai/src/components/Users/Users.jsx
+++ way_of_samurai/src/components/Users/Users.jsx
@@ -1,58 +1,90 @@
 import styles from './Users.module.css';
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 let Users = (props) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
+	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	let pages = [];
 
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-    return (
-        <div>
-            <div className={styles.pageDiv}>
-                 {pages.map(p => {
-                     return <span className={props.currentPage === p && styles.selectedPage || styles.page}
-                                  key={p}
-                                  onClick={() => {
-                                      props.onPageChanged(p);
-                                  }}>{p}</span>;
-                 })}
+	for (let i = 1; i <= pagesCount; i++) {
+		pages.push(i);
+	}
+	return (
+		<div>
+			<div className={styles.pageDiv}>
+				{pages.map(p => {
+					return <span className={props.currentPage === p && styles.selectedPage || styles.page}
+								 key={p}
+								 onClick={() => {
+									 props.onPageChanged(p);
+								 }}>{p}</span>;
+				})}
 
-            </div>
-            {props.users.map(user =>
+			</div>
+			{props.users.map(user =>
 
-                <div key={user.id}>
+				<div key={user.id}>
 
                     <span>
                         <div>
 							<NavLink to={'/profile/' + user.id}>
 							<img
-                                src={user.photos.small != null ? user.photos.small : 'https://i.pinimg.com/564x/73/bb/3c/73bb3c20168614bcafe98d781c8fbef6.jpg'}
-                                className={styles.userPhoto}/>
+								src={user.photos.small != null ? user.photos.small : 'https://i.pinimg.com/564x/73/bb/3c/73bb3c20168614bcafe98d781c8fbef6.jpg'}
+								className={styles.userPhoto}/>
 
 							</NavLink>
 							</div>
                         <div>
                             {user.followed
-                                ? <button onClick={() => {
-                                    props.unfollow(user.id);
-                                }}> Unfollow </button>
-                                : <button onClick={() => {
-                                    props.follow(user.id);
-                                }}> Follow </button>
-                            }
+								? <button onClick={() => {
+
+									//delete server data
+									axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`,
+										{
+											withCredentials: true,
+											headers: {
+												'API-KEY': '6923b870-8944-47cc-9df6-38538d743985'
+											}
+										}
+									)
+										.then(response => {
+												if (response.data.resultCode === 0) {
+													props.unfollow(user.id);
+												}
+											}
+										);
+								}}> Unfollow </button>
+								: <button onClick={() => {
+
+									//post server data
+									axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`,
+										{},
+										{
+											withCredentials: true,
+											headers: {
+												'API-KEY': '6923b870-8944-47cc-9df6-38538d743985'
+											}
+										}
+									)
+										.then(response => {
+												if (response.data.resultCode === 0) {
+													props.follow(user.id);
+												}
+											}
+										);
+								}}> Follow </button>
+							}
                         </div>
                                 </span>
-                    <span>
+					<span>
                                 <div>{user.name}</div>
                                 <div>{user.status}</div>
                                 </span>
-                </div>
-            )}
-        </div>
-    );
+				</div>
+			)}
+		</div>
+	);
 };
 export default Users;
